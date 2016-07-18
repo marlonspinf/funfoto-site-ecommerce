830b7279324e43b8e02944ebd7ef1ff14fff4746
marlon <marloso88@gmail.com>
2016-07-18 20:58:31 -0300

    First Commit
    Test Project

---
+++ testemavenweb/src/test/java/SuperUtilTest.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SuperUtilTest {
+
+    @Test
+    public void escapeHTMLTest() {
+
+        String escapedHtml = SuperUtil.escapeHTML("<script>alert(1);</script>");
+        String expected = "&lt;script&gt;alert(1);&lt;/script&gt;"; 
+        Assert.assertEquals(expected, escapedHtml);
+
+    }
+    
+    @Test 
+    public void testeVerdade(){
+    	
+    	Assert.assertEquals(1, 1);
+    	
+    }
+    
+    
+
+}
